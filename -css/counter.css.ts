305dd35078562c571b475e4136225cfd7d2dbb0c
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-09-12 23:51:24 +0300

    Init

---
+++ -css/counter.css.ts
@@ -0,0 +1,3 @@
+namespace $ { $mol_style_attach( "atlz/counter/counter.css",
+ "[atlz_theme=\"light\"] {\n\tbackground-color: white;\n}\n\n[atlz_theme=\"dark\"] {\n\tbackground-color: black;\n}\n"
+) }
---
+++ counter.ts
@@ -0,0 +1,71 @@
+class $atlz_counter extends $atlz_lom_view {
+	count(next?: number) {
+		return $atlz_counter_storage.value('count', next) ?? 0
+	}
+
+	count_str(next?: string) {
+		return this.count(next?.valueOf && Number(next)).toString()
+	}
+
+	inc() {
+		this.count(this.count() + 1)
+	}
+
+	dec() {
+		this.count(this.count() - 1)
+	}
+
+	// Создаем инстанс Button
+	// Переопределяем title
+	// click биндим на this.inc
+	_Inc = null as unknown as $atlz_lom_view
+	Inc() {
+		if (this._Inc) return this._Inc
+
+		const obj = new $atlz_lom_button()
+		obj.title = () => '+'
+		obj.click = () => this.inc()
+
+		return (this._Inc = obj)
+	}
+
+	_Dec = null as unknown as $atlz_lom_view
+	Dec() {
+		if (this._Dec) return this._Dec
+
+		const obj = new $atlz_lom_button()
+		obj.title = () => '-'
+		obj.click = () => this.dec()
+
+		return (this._Dec = obj)
+	}
+
+	_Count = null as unknown as $atlz_lom_view
+	Count() {
+		if (this._Count) return this._Count
+
+		const obj = new $atlz_lom_input()
+		obj.value = (next?: string) => this.count_str(next)
+
+		return (this._Count = obj)
+	}
+
+	sub() {
+		return [this.Dec(), this.Count(), this.Inc()]
+	}
+
+	static mount() {
+		if (typeof document === 'undefined') return // Костыль
+
+		const node = document.querySelector('#root')
+		const obj = new $atlz_counter()
+
+		node?.replaceWith(obj.dom_tree())
+
+		// Реактивность добавится в следующей главе, сейчас воспользуемся костылем
+		setInterval(() => obj.dom_tree(), 100)
+	}
+}
+
+// Вызываем для монтирования приложения в DOM-дерево
+$atlz_counter.mount()
