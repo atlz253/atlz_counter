6070782c28684ceed3b31557411b3224f8390383
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-09-13 00:05:43 +0300

    Delete -css directory

--- -css/counter.css.ts
+++
@@ -1,3 +0,0 @@
-namespace $ { $mol_style_attach( "atlz/counter/counter.css",
- "[atlz_theme=\"light\"] {\n\tbackground-color: white;\n}\n\n[atlz_theme=\"dark\"] {\n\tbackground-color: black;\n}\n"
-) }
